4f28b92470a1be6be1019fad2b0ac41061806df1
olkaveter <olkaveter@yandex.ru>
2023-12-23 17:45:53 +0300

    finish auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -15,7 +15,7 @@ const auth = async (req, res, next) => {
     req.user = user;
 
     next();
-  } catch (error  ) {
+  } catch (error) {
     res.status(401).json({ message: 'Не авторизован' });
   }
 }
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const router = express.Router();
 const { login, register, current } = require('../controllers/users');
 const { auth } = require('../middleware/auth');
-const router = express.Router();
 
 // /api/user/login
 router.post('/login', login);
